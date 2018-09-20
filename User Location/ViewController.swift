04ed733a72961114ed8a0c58c81634d6715c5972
Eder <edermitten@gmail.com>
2018-09-20 18:08:32 -0500

    extracting address

--- User Location/ViewController.swift
+++ User Location/ViewController.swift
@@ -28,17 +28,59 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let userLocation: CLLocation = locations[0]
-        let latitude = userLocation.coordinate.latitude
-        let longitude = userLocation.coordinate.longitude
+        //let latitude = userLocation.coordinate.latitude
+        //let longitude = userLocation.coordinate.longitude
         
-        let latDelta: CLLocationDegrees = 0.05
-        let lonDelta: CLLocationDegrees = 0.05
+        //let latDelta: CLLocationDegrees = 0.05
+        //let lonDelta: CLLocationDegrees = 0.05
         
-        let spam : MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
-        let coordinates : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let region : MKCoordinateRegion = MKCoordinateRegion(center: coordinates, span: spam)
+        //let spam : MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        //let location : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        //let region : MKCoordinateRegion = MKCoordinateRegion(center: coordinates, span: spam)
         
-        self.map.setRegion(region, animated: true)
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                if let placemark = placemarks?[0] {
+                    //print(placemark)
+                    var subThoroughfare = ""
+                    if placemark.subThoroughfare != nil {
+                        subThoroughfare = placemark.subThoroughfare!
+                    }
+                    
+                    var thoroughfare = ""
+                    if placemark.thoroughfare != nil {
+                        thoroughfare = placemark.thoroughfare!
+                    }
+                    
+                    var subLocality = ""
+                    if placemark.subLocality != nil {
+                        subLocality = placemark.subLocality!
+                    }
+                    
+                    var subAdministrativeArea = ""
+                    if placemark.subAdministrativeArea != nil {
+                        subAdministrativeArea = placemark.subAdministrativeArea!
+                    }
+                    
+                    var postalCode = ""
+                    if placemark.postalCode != nil {
+                        postalCode = placemark.postalCode!
+                    }
+                    
+                    var country = ""
+                    if placemark.country != nil {
+                        country = placemark.country!
+                    }
+                    
+                    print(subThoroughfare + thoroughfare + "\n" + subLocality + "\n" + subAdministrativeArea + "\n" + postalCode + "\n" + country)
+                    
+                }
+            }
+        }
+        
+        //self.map.setRegion(region, animated: true)
         /*
         let annotation = MKPointAnnotation()
         annotation.title = ""
