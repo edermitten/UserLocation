604deb2bf24d19e7cabc949193eba421c07a0acb
Eder <edermitten@gmail.com>
2018-09-20 16:28:30 -0500

    center the current location

--- User Location/ViewController.swift
+++ User Location/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet var map: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation: CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        
+        let spam : MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let coordinates : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region : MKCoordinateRegion = MKCoordinateRegion(center: coordinates, span: spam)
+        
+        self.map.setRegion(region, animated: true)
+        /*
+        let annotation = MKPointAnnotation()
+        annotation.title = ""
+        annotation.subtitle = ""
+        annotation.coordinate = coordinates
+        
+        self.map.addAnnotation(annotation)
+        */
+        
     }
 
 
