5e8ecac2dc1621aa894d69d7da02d88963c1d69e
Eder <edermitten@gmail.com>
2018-09-20 15:08:05 -0500

    create repository commit

---
+++ User Location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  User Location
+//
+//  Created by Eder Mitten on 9/20/18.
+//  Copyright © 2018 Eder Mitten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
